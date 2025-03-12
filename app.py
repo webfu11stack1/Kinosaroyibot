05d65c6f3e8abeef4666134630844eaeca2ec1f3
coder_darkweb <133632550+webfu11stack1@users.noreply.github.com>
2025-03-12 23:51:21 +0500

    app.py

--- app.py
+++ app.py
@@ -1253,7 +1253,7 @@ async def start(message: types.Message, state: FSMContext):
         keyboard = InlineKeyboardMarkup(row_width=1)
         for _, channel_url in channels:
             keyboard.add(InlineKeyboardButton(text="➕ Obuna bo'lish", url=channel_url))  
-        keyboard.add(InlineKeyboardButton(text="➕ Obuna bo'lish",url="https://t.me/+uTTB_laRnCVmMDhi"))
+        
         keyboard.add(InlineKeyboardButton(text="Instagram Sahifamiz",url="https://www.instagram.com/ar7.movie"))
         keyboard.add(InlineKeyboardButton(text="Tekshirish ✅", url="https://t.me/kinosaroyibot?start=True" ))
         
